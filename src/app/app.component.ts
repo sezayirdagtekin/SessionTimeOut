8792c614f55795bc003728d6ddaa1e60bf84343b
Dagtekin <212557891@logon.ds.ge.com>
2018-11-20 10:45:26 +0300

    add bootstap functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {ProgressBarModalComponent} from './progressbar-modal.component';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap/modal/modal.module';
+import {Keepalive} from '@ng-idle/keepalive';
+import {EventTargetInterruptSource, Idle} from '@ng-idle/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,109 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Session-Timeout';
+  title = 'Session Timeout Demo {ng-idle keepalive}';
+  idleState = 'NOT_STARTED';
+  timedOut = false;
+  lastPing?: Date = null;
+  progressBarPopup: NgbModalRef;
+  
+  
+  
+  constructor(private element: ElementRef, private idle: Idle, private keepalive: Keepalive, private ngbModal: NgbModal) {
+    // sets an idle timeout of 15 minutes.
+    idle.setIdle(20);
+    // sets a timeout period of 5 minutes.
+    idle.setTimeout(10);
+    // sets the interrupts like Keydown, scroll, mouse wheel, mouse down, and etc
+    idle.setInterrupts([
+      new EventTargetInterruptSource(
+        this.element.nativeElement, 'keydown DOMMouseScroll mousewheel mousedown touchstart touchmove scroll')]);
+
+    idle.onIdleEnd.subscribe(() => {
+      this.idleState = 'NO_LONGER_IDLE';
+    });
+
+    idle.onTimeout.subscribe(() => {
+      this.idleState = 'TIMED_OUT';
+      this.timedOut = true;
+      this.closeProgressForm();
+    });
+
+    idle.onIdleStart.subscribe(() => {
+      this.idleState = 'IDLE_START', this.openProgressForm(1);
+    });
+
+    idle.onTimeoutWarning.subscribe((countdown: any) => {
+      this.idleState = 'IDLE_TIME_IN_PROGRESS';
+      this.progressBarPopup.componentInstance.count = (Math.floor((countdown - 1) / 60) + 1);
+      this.progressBarPopup.componentInstance.progressCount = this.reverseNumber(countdown);
+      this.progressBarPopup.componentInstance.countMinutes = (Math.floor(countdown / 60));
+      this.progressBarPopup.componentInstance.countSeconds = countdown%60;
+    });
+
+    // sets the ping interval to 15 seconds
+    keepalive.interval(15);
+    /**
+     *  // Keepalive can ping request to an HTTP location to keep server session alive
+     * keepalive.request('<String URL>' or HTTP Request);
+     * // Keepalive ping response can be read using below option
+     * keepalive.onPing.subscribe(response => {
+     * // Redirect user to logout screen stating session is timeout out if if response.status != 200
+     * });
+     */
+ 
+
+   
+    this.reset();
+  }
+  
+  
+  
+  
+  
+  ngOnDestroy() {
+    this.resetTimeOut();
+
+  }
+
+  reverseNumber(countdown: number) {
+    return (300 - (countdown - 1));
+  }
+
+  reset() {
+    this.idle.watch();
+    this.idleState = 'Started.';
+    this.timedOut = false;
+  }
+
+  openProgressForm(count: number) {
+    this.progressBarPopup = this.ngbModal.open(ProgressBarModalComponent, {
+      backdrop: 'static',
+      keyboard: false
+    });
+    this.progressBarPopup.componentInstance.count = count;
+    this.progressBarPopup.result.then((result: any) => {
+      if (result !== '' && 'logout' === result) {
+        this.logout();
+      } else {
+        this.reset();
+      }
+    });
+  }
+
+  logout() {
+    this.resetTimeOut();
+  }
+
+  closeProgressForm() {
+    this.progressBarPopup.close();
+  }
+
+  resetTimeOut() {
+    this.idle.stop();
+    this.idle.onIdleStart.unsubscribe();
+    this.idle.onTimeoutWarning.unsubscribe();
+    this.idle.onIdleEnd.unsubscribe();
+    this.idle.onIdleEnd.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule } from '@angular/common/http';
+import {NgIdleKeepaliveModule} from '@ng-idle/keepalive';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ProgressBarModalComponent} from './progressbar-modal.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+	 AppComponent,
+    ProgressBarModalComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    NgIdleKeepaliveModule.forRoot(),
+    HttpModule,
+	HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ProgressBarModalComponent]
 })
 export class AppModule { }
